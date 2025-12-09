b6fc1a7a178418c1a511069084df4a8f927291fa
saloni <salonijaiswal390@gmail.com>
2025-12-09 16:43:13 +0530

    changs in content

--- Content.java
+++ Content.java
@@ -3,6 +3,7 @@ echo "# sib_loans_app" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
+saloni jaiswal
 git branch -M main
 git remote add origin https://github.com/saloni186/sib_loans_app.git
 git push -u origin main}
